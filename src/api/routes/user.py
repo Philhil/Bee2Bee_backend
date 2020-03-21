0b098f0324e4aa8e74cbbde744188c471ffc556f
Vinzenz Sinapius <vinz@core.localdomain>
2020-03-22 00:36:08 +0100

    refactor user.py

--- src/api/routes/user.py
+++ src/api/routes/user.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, request, jsonify, abort
-from models import db
+from models import db, get_table
 
 from sqlalchemy.sql import select
 from sqlalchemy import Table
 
 user_api = Blueprint(__name__, 'api', url_prefix="/api/v0/user" )
 
-@user_api.route('/', methods=['post', 'put'])
+@user_api.route('/', methods=['POST'])
 def user_data():
     if not request.json:
         abort(400)
@@ -20,18 +20,26 @@ def user_data():
 
     json_data = data["body"]
 
-    if request.method == 'POST':
-        conn = db.session.connection()
-        user = Table('user', db.metadata, autoload=True, autoload_with=db.engine)
-        s = user.insert().values(firstname= json_data.get('first_name'), lastname=json_data.get('name'), email=json_data.get('email'), pwd=json_data.get('secret'), company_id=json_data.get('company_id'))
-        result = conn.execute(s)
-        output_user = {}
-        output_user['id'] = result.inserted_primary_key
-        output_user['name'] = json_data.get('name')
-        output_user['first_name'] = json_data.get('first_name')
-        output_user['secret'] = json_data.get('secret')
-        output_user['email'] = json_data.get('email')
-        output_user['company_id'] = json_data.get('company_id')
-        db.session.commit()
+    
+    with db.engine.begin() as conn:        
+        user = get_table('user')
+        ins = user.insert().values(
+            email=json_data.get('email'), 
+            firstname=json_data.get('first_name'), 
+            lastname=json_data.get('name'),         
+            pwd=json_data.get('secret'), 
+            company_id=json_data.get('company_id')
+        )
+        result = conn.execute(ins)
 
-        return jsonify(output_user)
+
+    output_user = {}
+    output_user['id'] = result.inserted_primary_key[0]
+    output_user['name'] = json_data.get('name')
+    output_user['first_name'] = json_data.get('first_name')
+    output_user['secret'] = json_data.get('secret')
+    output_user['email'] = json_data.get('email')
+    output_user['company_id'] = json_data.get('company_id')
+    
+
+    return jsonify(output_user)
