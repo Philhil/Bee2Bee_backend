a84a03feba50feb82431bb53d97f068fda6b1460
Vinzenz Sinapius <vinz@core.localdomain>
2020-03-22 00:09:12 +0100

    add address info to company creation

--- src/api/routes/company.py
+++ src/api/routes/company.py
@@ -5,7 +5,7 @@ from models import db, get_table
 company_api = Blueprint(__name__, 'api', url_prefix="/api/v0/company")
 
 
-# test with http POST localhost:5000/api/v0/company body:='{"name": "Bar Ltd"}'
+# test with http POST localhost:5000/api/v0/company/ body:='{"name": "Bar Ltd"}'
 @company_api.route('/', methods=['POST'])
 def create_company():
     if not request.json:
@@ -19,13 +19,36 @@ def create_company():
         abort(405) 
 
     name = data["body"]["name"]
+    address_data = {}
+    has_address_info = False
+    address_keys = ['zip_code', 'street', 'house_nr', 'city', 'state', 'country']
+    for address_key in address_keys:
+        if address_key in data["body"]:
+            has_address_info = True        
+        address_data[address_key] = data["body"].get(address_key, None)
+    
+    conn = db.session.connection() 
     company = get_table('company')
+    address = get_table('address')
+    
+    address_id = None
+    if has_address_info:
+        ins = address.insert().values(
+            **address_data
+        )
+        result = conn.execute(ins)
+        address_id = result.inserted_primary_key[0]
+
+    company_data = {
+        "name": name
+    }
+    if has_address_info:
+        company_data['address_id'] = address_id
 
     ins = company.insert().values(name=name)
-    conn = db.session.connection()        
     result = conn.execute(ins)
     db.session.commit()
-    return jsonify({
-        "id": result.inserted_primary_key[0],
-        "name": name
-        })
+
+    company_data['id'] = result.inserted_primary_key[0]
+
+    return jsonify(company_data)
