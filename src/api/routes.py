9f4e65470ad0605cd086ace3aac57c29a0981d36
Vinzenz Sinapius <vinz@core.localdomain>
2020-03-21 22:52:53 +0100

    post company route

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, abort
+from models import db, get_table
 api = Blueprint(__name__, 'api', url_prefix="/api/v0/" )
 
 @api.route('/')
 def home():
     return 'Hello World!'
+
+
+# test with http POST localhost:5000/api/v0/company body:='{"name": "Bar Ltd"}'
+@api.route('/company', methods=['POST'])
+def create_company():
+    if not request.json:
+        abort(400)
+    data = request.json
+    if not "body" in data:
+        abort(405)   
+    if not "name" in data["body"]:
+        abort(405)    
+    if len(data["body"]["name"]) == 0:
+        abort(405) 
+
+    name = data["body"]["name"]
+    company = get_table('company')
+
+    ins = company.insert().values(name=name)
+    conn = db.session.connection()        
+    result = conn.execute(ins)
+
+    return jsonify({
+        "id": result.inserted_primary_key,
+        "name": name
+        })
