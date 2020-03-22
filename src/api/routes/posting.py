d00c8fda0108a107eab870920f704fc9d2df8a08
Stephan Ferenz <ferenz@ei.uni-hannover.de>
2020-03-22 17:02:16 +0100

    Added put for posting

--- src/api/routes/posting.py
+++ src/api/routes/posting.py
@@ -113,9 +113,73 @@ def create_position():
     })
 
 @posting_api.route('/', methods=['PUT'])
+# test http PUT localhost:5000/api/v0/posting/ body:='{"id": 8, "posting_type":1, "title": "testtile", "description": "Testbeschreibung", "address": {"zip_code": "83730", "city": "Fischbachau", "street": "Im Rothmoos", "number": "12"}, "skills": [1,2]}'
 def modify_position():
-    # TODO
-    abort(400)
+    if not request.json:
+        abort(400)
+    data = request.json
+    if not "body" in data:
+        abort(405)
+
+    # Check for required keys
+    requiredKeys = ["id"]
+    for key in requiredKeys:
+        if not key in data["body"]:
+            abort(405)
+    posting_id = data["body"]['id']
+
+    # Convert input data
+    new_data = {}
+    for key, value in data['body'].items():
+        if key == 'price':
+            new_data['wage_hourly']=value
+        elif key == 'num_people':
+            new_data['num_pers'] = value
+        elif key == 'posting_type':
+            new_data['state_id'] = value
+        elif key == 'travel_ability_needed':
+            new_data['traveling'] = value
+        elif key == 'travel_radius':
+            new_data['travel_radius'] = value
+        elif key != 'address':
+            new_data[key] = value
+
+    with db.engine.begin() as conn:
+        position = get_table('position')
+        update = position.update().where(position.c.id==data["body"]["id"]).values(**new_data)
+        result = conn.execute(update)
+
+    result_posting = result.rowcount
+
+    if "address" in data["body"]:
+        # Get address id
+        with db.engine.begin() as conn:
+            position = get_table('position')
+            sel = select([position.c.address_id],
+                position.c.id == posting_id
+            )
+            result = conn.execute(sel)
+            address_id = result.fetchone()[0]
+
+        address_data = geocoding(
+            data["body"]["address"].get("number"),
+            data["body"]["address"].get("street"),
+            data["body"]["address"].get("city"),
+            data["body"]["address"].get("zip_code"),
+            data["body"]["address"].get("state", "Deutschland"),
+            data["body"]["address"].get("country", "de-DE")
+            )
+
+        with db.engine.begin() as conn:
+            address = get_table('address')
+            update = address.update().where(address.c.id==address_id).values(**address_data)
+            result = conn.execute(update)
+
+        result_address = result.rowcount
+
+    if result_address + result_posting == 0:
+        abort(404)
+    return jsonify(True)
 
 @posting_api.route('/<int:posting_id>/', methods=['GET'])
 # test with http GET localhost:5000/api/v0/posting/12/
